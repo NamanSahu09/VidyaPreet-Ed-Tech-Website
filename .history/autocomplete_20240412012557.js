ff29b9e42958aeb76f07673afedf052f8746e6b8
Naman Swastik Sahu <namanswastik11@gmail.com>
2024-04-12 12:00:53 +0530

    bsc.html added with functionality in search

---
+++ .history/autocomplete_20240412012557.js
@@ -0,0 +1,58 @@
+let availableKeywords = [
+  "Indian Institute of Technology Bombay",
+  "KIIT UNIVERSITY",
+  'SOA University',
+  'Trident College',
+  'Silicon College',
+  'BCA College',
+  'BSc. College',
+  'B. Tech College',
+  'Amity University',
+  'Manipal University',
+  'Jawaharlal Nehru University',
+  'VIT University',
+  'SRM University',
+  'Biju Patnaik University of Technology',
+  'Utkal university',
+  'Ravenshaw University',
+  'Sri Sri University',
+  'Odisha University of Agriculture and Technology, Bhubaneswar',
+  'Veer Surendra Sai University of Technology (VSSUT), Burla, Sambalpur',
+  'GIET University'
+];
+
+const resultBox = document.querySelector('.result-box');
+const inputBox = document.querySelector('#input-box');
+
+inputBox.onkeyup = function() {
+  let input = inputBox.value;
+  let result = [];
+
+  if (input.length) {
+    result = availableKeywords.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+  }
+
+  const content = result.map((list) => {
+    return `<li onclick=selectInput(this)>${list}</li>`;
+  });
+
+  resultBox.innerHTML = '<ul>' + content.join('') + '</ul>';
+}
+
+function selectInput(element) {
+  inputBox.value = element.innerHTML;
+  resultBox.innerHTML = '';
+}
+function searchGoogle() {
+  const inputBox = document.getElementById('input-box');
+  const searchTerm = inputBox.value.trim();
+  if (searchTerm !== '')
+   {
+      // const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchTerm);
+      const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchTerm);
+      window.location.href = searchUrl.html;
+
+  }
+}
